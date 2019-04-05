7d643943bde14d2502d0dc5dbf0ec4f6053139f1
Nina Boosheri <nboosheri@gmail.com>
2019-04-05 15:01:45 -0700

    create home and about path/pages

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+
+    # methods inside controllers are called `actions`
+    # so this is the `index` action
+    
+    def index
+        render "index"
+         end
+
+    
+    end
+    
